155f2681bd5560f12383c01d5efb2293904f6cd3
Aman soni <amansoniabesgmail.com>
2021-07-03 19:36:39 +0530

    files added

---
+++ program.py
@@ -0,0 +1,51 @@
+
+# Python Program to Reverse a Number
+#
+# num = 1223456
+# print(str(num)[::-1])
+
+
+#Python Program to Compute the Power of a Number
+
+# num = int(input("Enter a num : "))
+# power = int(input("Enter power: "))
+# result = num**power
+# print(f"{num} to the power is {power}: {result}")
+
+
+# Python Program to Count the Number of Digits Present In a Number
+
+# num = int(input("Enter a Digit : "))
+# count = 0
+# if num !=0:
+#     num=num//10
+#     count=count +1
+#     print(count)
+# elif
+
+# print(count)
+
+
+# Python Program to Check If Two Strings are Anagram
+# str1= (input("Enter first String : "))
+# str2= (input("Enter Second String :"))
+# str_1=str1.lower()
+# str_2=str2.lower()
+#
+# if len(str_1) == len(str_2):
+#     sort1=sorted(str_1)
+#     sort2=sorted(str_2)
+#     if sort1 == sort2:
+#         print("value is anagram ")
+#     else:
+#         print(f"value is not anagram")
+# else:
+#     print("value is not anagram")
+
+
+#Python Program to Capitalize the First Character of a String
+
+# str1 = "Aman is a good boy"
+# print(str1.capitalize()
+
+# Python Program to Compute all the Permutation of the String
